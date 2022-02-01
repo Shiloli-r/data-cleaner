fcb6142803180d5a143a7c9d4dea8bc92320e41d
Shiloli-r <ronaldshiloli@gmail.com>
2022-02-01 11:48:54 +0300

    log in page

--- cleaner/web/views.py
+++ cleaner/web/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, 'index.html', context)
+
+
+def login(request):
+    context = {}
+    return render(request, 'login.html', context)
